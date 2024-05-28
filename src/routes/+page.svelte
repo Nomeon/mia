d72d43aeeccd91a36dcfdbdb7debc146ad89061d
Stijn Nijhuis <spg.nijhuis@hotmail.com>
2024-05-28 15:51:23 +0200

    Add tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,48 @@
 <script lang='ts'>
-    import { onMount } from "svelte";
-    import { handleIFC } from "$lib/ifc/ifc-handler";
-    import { createLVL } from "$lib/lvl/createLVL";
-    import { createBP } from "$lib/bp/createBP";
-    import { createISO } from "$lib/iso/createISO";
-    import { createFOL } from "$lib/fol/createFOL";
-    import { createOLI } from "$lib/oli/createOLI";
-    import * as Excel from 'exceljs';
+  import { onMount } from "svelte";
+  import { handleIFC } from "$lib/ifc/ifc-handler";
+  import { createLVL } from "$lib/lvl/createLVL";
+  import { createBP } from "$lib/bp/createBP";
+  import { createISO } from "$lib/iso/createISO";
+  import { createFOL } from "$lib/fol/createFOL";
+  import { createOLI } from "$lib/oli/createOLI";
+  import * as Excel from 'exceljs';
 	import type { IfcElement } from "$lib/ifc/ifc-types";
-
+  import { Button } from "$lib/components/ui/button";
     // TODO: Olivijn kg + m2, Check isolatie, check volume ipv inhoud, aggregatie 
 
-    onMount(async () => {
-        const response = await fetch('/test.ifc');
-        const fileContent = await response.arrayBuffer();
-        let rawData = new Uint8Array(fileContent);
-        const elements = await handleIFC(rawData);
-        createExcel(elements);
-    });
+  onMount(async () => {
+    const response = await fetch('/test.ifc');
+    const fileContent = await response.arrayBuffer();
+    let rawData = new Uint8Array(fileContent);
+    const elements = await handleIFC(rawData);
+    createExcel(elements);
+  });
 
-    function createExcel(elements: IfcElement[]) {
-        const workbook = new Excel.Workbook();
-        createLVL(workbook, elements);
-        createBP(workbook, elements);
-        createISO(workbook, elements);
-        createFOL(workbook, elements);
-        createOLI(workbook, elements);
-        createDownload(workbook);
-    }
+  function createExcel(elements: IfcElement[]) {
+    const workbook = new Excel.Workbook();
+    createLVL(workbook, elements);
+    createBP(workbook, elements);
+    createISO(workbook, elements);
+    createFOL(workbook, elements);
+    createOLI(workbook, elements);
+    // createDownload(workbook);
+  }
 
-    function createDownload(wb: Excel.Workbook) {
-        wb.xlsx.writeBuffer().then((data) => {
-            const blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-            const url = URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = 'test.xlsx';
-            a.click();
-            URL.revokeObjectURL(url);
-        });
-    }
+  function createDownload(wb: Excel.Workbook) {
+    wb.xlsx.writeBuffer().then((data) => {
+      const blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'test.xlsx';
+      a.click();
+      URL.revokeObjectURL(url);
+   });
+  }
 </script>
+
+<div class='p-2'>
+  <span>a</span>
+  <Button>Test</Button>
+</div>
